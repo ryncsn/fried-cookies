c51512109d202321db5ef3f1801b787868eb2cde
Kairui Song <kasong@redhat.com>
2018-05-06 21:11:22 +0800

    Static-Memory-Pool.cpp

--- cpp-snips/Static-Memory-Pool.cpp
+++ cpp-snips/Static-Memory-Pool.cpp
@@ -22,30 +22,6 @@ using namespace::std;
 #define N_MAX 100000
 
 
-vector<int> to[N_MAX];
-int match[N_MAX] = {-1};
-bool visited[N_MAX] = {false};
-bool skip[N_MAX] = {0};
-
-bool bi_match_dfs(int num) {
-	/* Do DFS bi match in place */
-	if (skip[num]) {
-		return false;
-	}
-	for (int next : to[num]) {
-		if (visited[next] == false && !skip[next]) {
-			visited[next] = true;
-			if (match[next] == -1 || bi_match_dfs(match[next])) {
-				match[num] = next;
-				match[next] = num;
-				return true;
-			}
-		}
-	}
-	return false;
-}
-
-
 // SIZE, STEP: unit: Byte
 template <const size_t SIZE = 32768 * 1024, const size_t STEP = 1024 * 1024>
 struct StaticMemoryPool {
